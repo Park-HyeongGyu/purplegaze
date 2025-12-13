8596824440b8d527b2e5f3a53ea9efc9d3dc9b3c
Hyeonggyu Park <phg7889@gmail.com>
2025-12-14 04:01:25 +0900

    Add send_telegram func

--- include/telegram.h
+++ include/telegram.h
@@ -1,4 +1,16 @@
 #ifndef TELEGRAM_H
 #define TELEGRAM_H
 
+#include "telegram/bot.h"
+
+typedef enum{
+    TG_OK = 0,
+
+    TG_ERR_VALID, // Validation failed
+    TG_ERR_PREP, // Rreparation of paylord and url failed
+    TG_ERR_TRANS // Transporting http failed
+} tg_err_t;
+
+tg_err_t send_telegram(const char *message, const bot_t *mybot);
+
 #endif /* TELEGRAM_H */
